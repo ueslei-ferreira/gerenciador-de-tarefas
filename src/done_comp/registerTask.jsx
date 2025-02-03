70d3132f968593d64e65de14f4494aca20b7ebab
Ueslei GIT <ueslei392@gmail.com>
2025-02-02 22:58:04 -0300

    1.0

--- src/done_comp/registerTask.jsx
+++ src/done_comp/registerTask.jsx
@@ -1,30 +1,54 @@
 import React from "react";
-import { Box, Flex, Text, Stack, Link , Icon} from "@chakra-ui/react";
+import { Box, Stack, Input, Textarea } from "@chakra-ui/react";
+import {
+  SelectContent,
+  SelectItem,
+  SelectLabel,
+  SelectRoot,
+  SelectTrigger,
+  SelectValueText,
+} from "@/components/ui/select";
+import { Field } from "@/components/ui/field";
 
-const register_task = () =>{
+const frameworks = [
+  { label: "React.js", value: "react" },
+  { label: "Vue.js", value: "vue" },
+  { label: "Angular", value: "angular" },
+  { label: "Svelte", value: "svelte" },
+];
 
-    return (
+const RegisterTask = () => {
+  return (
+    <Stack direction="column" w="500px">
+      <Box h={"25%"}>
+        <SelectRoot size="sm" width="320px">
+          <SelectLabel>Select framework</SelectLabel>
+          <SelectTrigger>
+            <SelectValueText placeholder="Select framework" />
+          </SelectTrigger>
+          <SelectContent>
+            {frameworks.map((framework) => (
+              <SelectItem key={framework.value} value={framework.value}>
+                {framework.label}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </SelectRoot>
+      </Box>
+      <Box h={"25%"}>
+        <Field.Root>
+          <Field.Label>Title</Field.Label>
+          <Input placeholder="Ex. Bug Fix" />
+        </Field.Root>
+      </Box>
+      <Box h={"50%"}>
+        <Field.Root>
+          <Field.Label>Designation</Field.Label>
+          <Textarea />
+        </Field.Root>
+      </Box>
+    </Stack>
+  );
+};
 
-        <Stack direction="column" w="500px">
-            <Box h={"25%"}>
-                
-            </Box>
-            <Box h={"25%"}>
-                <Field.Root>
-                    <Field.Label>Title</Field.Label>
-                    <Input placeholder="Ex. Bug Fix" />
-                </Field.Root>
-            </Box>
-            <Box h={"50%"}>
-            <Field.Root>
-                <Field.Label>Designation</Field.Label>
-                <Textarea />
-            </Field.Root>
-            </Box>
-        </Stack>
-
-    );
-
-}
-
-export default register_task;
+export default RegisterTask;
